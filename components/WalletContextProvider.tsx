d46d52de0a4dcd30562192be6f930fd007f2e1a5
Thomas Gehrmann <s6thgehr@gmail.com>
2022-11-25 14:47:05 +0100

    Fix WalletModalProvider hydration error

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -1,29 +1,37 @@
-import { FC, ReactNode, useMemo } from "react"
+import { FC, ReactNode, useMemo } from "react";
 import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react"
-import * as web3 from "@solana/web3.js"
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
-import * as walletAdapterWallets from "@solana/wallet-adapter-wallets"
-require("@solana/wallet-adapter-react-ui/styles.css")
+    ConnectionProvider,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import * as web3 from "@solana/web3.js";
+import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
+require("@solana/wallet-adapter-react-ui/styles.css");
+import dynamic from "next/dynamic";
+
+const ReactUIWalletModalProviderDynamic = dynamic(
+    async () =>
+        (await import("@solana/wallet-adapter-react-ui")).WalletModalProvider,
+    { ssr: false }
+);
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const endpoint = web3.clusterApiUrl("devnet")
-  const wallets = useMemo(() => {
-    return [
-      new walletAdapterWallets.PhantomWalletAdapter(),
-      new walletAdapterWallets.SolflareWalletAdapter(),
-    ]
-  }, [])
+    const endpoint = web3.clusterApiUrl("devnet");
+    const wallets = useMemo(() => {
+        return [
+            new walletAdapterWallets.PhantomWalletAdapter(),
+            new walletAdapterWallets.SolflareWalletAdapter(),
+        ];
+    }, []);
 
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect={true}>
-        <WalletModalProvider>{children}</WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  )
-}
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect={true}>
+                <ReactUIWalletModalProviderDynamic>
+                    {children}
+                </ReactUIWalletModalProviderDynamic>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
 
-export default WalletContextProvider
+export default WalletContextProvider;
