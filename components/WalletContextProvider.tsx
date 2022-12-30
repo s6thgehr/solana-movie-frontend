acc1f0e5b7e455c685460891b66f1919736180e3
Thomas Gehrmann <s6thgehr@gmail.com>
2022-12-30 13:45:11 +0100

    Use devnet, because localhost does not show minted tokens

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -9,8 +9,8 @@ import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    // const endpoint = web3.clusterApiUrl("devnet")
-    const endpoint = "http://localhost:8899"; //web3.clusterApiUrl("devnet")
+    const endpoint = web3.clusterApiUrl("devnet");
+    // const endpoint = "http://localhost:8899"; //web3.clusterApiUrl("devnet")
     const wallets = [
         new walletAdapterWallets.PhantomWalletAdapter(),
         new walletAdapterWallets.SolflareWalletAdapter(),
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,2 @@
 export const MOVIE_REVIEW_PROGRAM_ID =
-    "AjH8jYtcEJQkH4EASaVTD6CooafSbkNXjyRUwzhz2ymu";
+    "EqwyBUMYnWiKBFu1egCBd6cGzjJNwq8AQdGVdSMoXVze";
